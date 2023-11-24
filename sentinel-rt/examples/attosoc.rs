414d96043e6747022c12385e9a2214500b079aa8
William D. Jones <thor0505@comcast.net>
2023-11-24 17:23:37 -0500

    Clean up static vars by using atomics and MaybeUninit.

--- sentinel-rt/examples/attosoc.rs
+++ sentinel-rt/examples/attosoc.rs
@@ -4,25 +4,21 @@
 use panic_halt as _;
 use riscv_rt::entry;
 use riscv::register::{mie, mstatus};
+use core::mem::MaybeUninit;
 use core::ptr::{write_volatile, read_volatile};
 use core::cell::{Cell, UnsafeCell};
 use critical_section::{self, Mutex};
 use heapless::spsc::{Queue, Consumer};
+use portable_atomic::{AtomicBool, AtomicU8, Ordering::SeqCst};
 
-
-#[derive(Clone, Copy)]
-enum TxState {
-    Sending,
-    Empty
-}
 
 static RX: Mutex<Cell<Option<u8>>> = Mutex::new(Cell::new(None));
-static TX: Mutex<Cell<TxState>> = Mutex::new(Cell::new(TxState::Empty));
-static TIMER: Mutex<Cell<bool>> = Mutex::new(Cell::new(false));
-static COUNT: Mutex<Cell<u8>> = Mutex::new(Cell::new(0));
+static TX_IN_PROGRESS: AtomicBool = AtomicBool::new(false);
+static TIMER: AtomicBool = AtomicBool::new(false);
+static COUNT: AtomicU8 = AtomicU8::new(0);
 // SAFETY: Emulating a "Send". We never touch this from non interrupt thread
 // once this is set.
-static TX_FIFO_C: Mutex<UnsafeCell<Option<Consumer<'static, u8, 64>>>> = Mutex::new(UnsafeCell::new(None));
+static mut TX_CONS: MaybeUninit<Consumer<'static, u8, 64>> = MaybeUninit::uninit();
 
 
 // `read/write_volatile` SAFETYs: Interrupts are disabled, valid I/O port
@@ -34,19 +30,14 @@ fn MachineExternal() {
     let tim_int: u8 = unsafe { read_volatile(0x04000000 as *const u8) };
 
     if (tim_int & 0x01) != 0 {
-        critical_section::with(|cs| {
-            let mut cnt = COUNT.borrow(cs).get();
-            cnt += 1;
+        let cnt = COUNT.fetch_add(1, SeqCst);
 
-            // Interrupts 12000000/16834, or ~764 times per second. Tone it
-            // down to 1/10th of a second.
-            if cnt == 73 {
-                cnt = 0;
-                TIMER.borrow(cs).set(true);
-            }
-
-            COUNT.borrow(cs).set(cnt);
-        });
+        // Interrupts 12000000/16834, or ~764 times per second. Tone it
+        // down to 1/10th of a second.
+        if cnt == 72 {
+            COUNT.store(0, SeqCst);
+            TIMER.store(true, SeqCst)
+        }
     }
 
     let ser_int = unsafe { read_volatile(0x06000001 as *const u8) };
@@ -58,27 +49,23 @@ fn MachineExternal() {
     }
 
     if (ser_int & 0x02) != 0 {
-        let maybe_tx = critical_section::with(|cs| {
-            TX.borrow(cs).get()
-        });
-
         let maybe_queue = critical_section::with(|cs| {
             // SAFETY: No other thread ever touches this. We cannot reach this
-            // line before main finishes initializing this var.
-            let cons = unsafe { (&mut *TX_FIFO_C.borrow(cs).get()).as_mut().unwrap() };
+            // line before main finishes initializing this var. Thus, this
+            // is the only &mut released to safe code.
+            let mut cons = unsafe { TX_CONS.assume_init_mut() };
             cons.dequeue()
         });
 
-        match (maybe_tx, maybe_queue) {
-            (TxState::Sending, Some(tx)) => {
-                unsafe { write_volatile(0x06000000 as *mut u8, tx) };
+        if TX_IN_PROGRESS.load(SeqCst) {
+            match maybe_queue {
+                Some(tx) => {
+                    unsafe { write_volatile(0x06000000 as *mut u8, tx) };
+                }
+                None => {
+                    TX_IN_PROGRESS.store(false, SeqCst) 
+                },
             }
-            (TxState::Sending, None) => {
-                critical_section::with(|cs| {
-                    TX.borrow(cs).set(TxState::Empty);
-                });
-            },
-            (TxState::Empty, _) => {}
         }
     }
 
@@ -97,10 +84,8 @@ fn main() -> ! {
         static mut Q: Queue<u8, 64> = Queue::new();
         unsafe { &mut Q }
     };
-    let (mut producer, consumer) = queue.split();
-    critical_section::with(|cs| {
-        unsafe { *TX_FIFO_C.borrow(cs).get() = Some(consumer) };
-    });
+    let (mut tx_prod, consumer) = queue.split();
+    unsafe { TX_CONS.write(consumer) };
 
     // SAFETY: Interrupts are disabled.
     unsafe {
@@ -114,16 +99,11 @@ fn main() -> ! {
        critical_section::with(|cs| {
             match RX.borrow(cs).get() {
                 Some(rx) => {
-                    match TX.borrow(cs).get() {
-                        TxState::Sending => {
-                            producer.enqueue(rx);
-                            // TX handler will run when finished sending.
-                        }
-                        TxState::Empty => {
-                            unsafe { write_volatile(0x06000000 as *mut u8, rx) };
-                            TX.borrow(cs).set(TxState::Sending);
-                            // TX handler will run when finished sending.
-                        }
+                    if TX_IN_PROGRESS.load(SeqCst) {
+                        tx_prod.enqueue(rx);
+                    } else {
+                        unsafe { write_volatile(0x06000000 as *mut u8, rx) };
+                        TX_IN_PROGRESS.store(true, SeqCst)
                     }
 
                     RX.borrow(cs).set(None);
@@ -131,20 +111,15 @@ fn main() -> ! {
                 None => {}
             }
 
-            if TIMER.borrow(cs).get() {
-                match TX.borrow(cs).get() {
-                    TxState::Sending => {
-                        producer.enqueue('T' as u8);
-                        // TX handler will run when finished sending.
-                    },
-                    TxState::Empty => {
-                        unsafe { write_volatile(0x06000000 as *mut u8, 'T' as u8) };
-                        TX.borrow(cs).set(TxState::Sending);
-                        // TX handler will run when finished sending.
-                    }
+            if TIMER.load(SeqCst) {
+                if TX_IN_PROGRESS.load(SeqCst) {
+                    tx_prod.enqueue('T' as u8);
+                } else {
+                    unsafe { write_volatile(0x06000000 as *mut u8, 'T' as u8) };
+                    TX_IN_PROGRESS.store(true, SeqCst)
                 }
 
-                TIMER.borrow(cs).set(false);
+                TIMER.store(false, SeqCst);
             }
 
             // Mirror the input port to the LEDs.
