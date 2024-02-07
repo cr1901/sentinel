9b4f691116b135d61cf625a49d422dda67654875
William D. Jones <thor0505@comcast.net>
2024-02-07 01:10:03 -0500

    Add stub documentation.

---
+++ doc/conf.py
@@ -0,0 +1,42 @@
+# ruff: noqa: D100
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'sentinel'
+copyright = '2024, William D. Jones'
+author = 'William D. Jones'
+release = '0.1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["myst_parser",
+              "sphinx.ext.autodoc",
+              "sphinx.ext.intersphinx",
+              "sphinx_rtd_theme",
+              "sphinx.ext.doctest",
+              "sphinx.ext.napoleon",
+              "sphinx.ext.todo"]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+intersphinx_mapping = {'python': ('https://docs.python.org/3', None),
+                       'amaranth': ('https://amaranth-lang.org/docs/amaranth/latest/', None)}  # noqa: E501
+autodoc_default_options = {"members": True,
+                           "undoc-members": True}
+todo_include_todos = True
+
+myst_footnote_transition = False
+myst_heading_anchors = 3
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_rtd_theme"
+html_static_path = ['_static']
---
+++ doc/index.md
@@ -0,0 +1,26 @@
+<!-- Sentinel documentation master file, created by
+   sphinx-quickstart on Wed Feb  7 00:04:29 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive. -->
+
+# Welcome to Sentinel's documentation!
+
+
+```{toctree}
+:maxdepth: 2
+:caption: "Contents:"
+
+usage/installation
+usage/quickstart
+usage/reference
+development/internals
+development/microcode
+development/testing
+```
+
+
+# Indices and tables
+
+* [](genindex)
+* [](modindex)
+* [](search)
